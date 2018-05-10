07e07dcb69f67a3699087dd3dfccc95dc7218a9a
srblimp <srblimp@srblimp-UX303LB>
2018-05-10 20:12:06 +0200

    Created MachineComponent abstract class

---
+++ src/MachineComponent.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author srblimp
+ */
+public abstract class MachineComponent {
+    protected boolean broken = false;
+    
+    public void setBroken() { broken = true; }
+    public void repair() { broken = false; }
+    public abstract boolean isBroken();
+}
