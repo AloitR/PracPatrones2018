c1fb93fb9bceff19e3b0e50f964016fe369e925f
SrBlimp <srblimp@gmail.com>
2018-05-10 20:22:30 +0200

    Created MachineComposite class

---
+++ src/MachineComposite.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author srblimp
+ */
+public class MachineComposite extends MachineComponent {
+    private List<MachineComponent> components = new ArrayList<>();
+    
+    public void addComponent(MachineComponent mc) {
+        components.add(mc);
+    }
+    
+    public boolean isBroken() {
+        if (broken) { return true; }
+        for (MachineComponent mc: components) {
+            if (mc.isBroken()) { return true; }
+        }
+        return false;
+    }
+}
