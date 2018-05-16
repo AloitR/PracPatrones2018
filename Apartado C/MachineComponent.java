f333a5eaebcc2d0aa4407a59142183c51ed4892f
Aloit <aloitr@gmail.com>
2018-05-16 16:05:12 +0200

    Updated folders & packages.

---
+++ Apartado C/MachineComponent.java
@@ -0,0 +1,12 @@
+package ApartatC;
+
+import java.util.Observable;
+
+public abstract class MachineComponent extends Observable
+{
+    protected boolean broken = false;
+    
+    public void setBroken() { broken = true; }
+    public void repair() { broken = false; }
+    public abstract boolean isBroken();
+}
